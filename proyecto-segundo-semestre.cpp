bd1ea84685e7cac221c7b164f3e87c985b9e3546
Diana <aztecvoyagediana@gmail.com>
2025-05-12 09:30:58 -0700

    Update proyecto-segundo-semestre.cpp
    cambie el orden de clases para poder utiliar consecuencias, dependiendo de los niveles de emocion de corazoncito se aplica una frase random para 8 segundos, y depsues limpia la pantalla

--- proyecto-segundo-semestre.cpp
+++ proyecto-segundo-semestre.cpp
@@ -2,22 +2,23 @@
 // Programación orientada a objetos
 // Denzel Isaac Uriarte Munoz - 34684
 // Carlos 
-// Diana G. - 38239
+// Diana - 38239
 // Diego
 
 
 #include <iostream>
 #include <string>
-#include <ctime>   
-#include <cstdlib> 
-#include <vector>
+#include <ctime>   //para funcion rand y srand
+#include <cstdlib> // para funcion de system(CLS) {limpia la pantalla}
+#include <vector> //manipular vector containers
+#include <windows.h> //para la funcion Sleep()
 
 using namespace std;
 
 
 class Emocion {
-    int nivelAnimo ;
-    int nivelEstres ;
+    int nivelAnimo;
+    int nivelEstres;
 
 public:
 
@@ -26,7 +27,7 @@ public:
     int getNivelAnimo() const { return nivelAnimo; }
     int getNivelEstres() const { return nivelEstres; }
 
-    
+
     void setNivelAnimo(int animo) { nivelAnimo = animo; }
     void setNivelEstres(int estres) { nivelEstres = estres; }
 
@@ -39,6 +40,75 @@ public:
 
 };
 
+class Consecuencia {
+protected:
+    vector<string> frases;
+
+public:
+
+    Consecuencia() { }
+
+    virtual ~Consecuencia() {}
+
+    virtual void mostrarFrase(const Emocion& emocion) { //uso de override para llamar either consecuencia negativa o positiva
+        int estres = emocion.getNivelEstres(); //saca los niveles de estres
+        int animo = emocion.getNivelAnimo();
+
+        int indiceDeFrases = rand() % frases.size(); // selecionna un numero random de frases
+        cout << frases[indiceDeFrases] << endl; //desplega
+        Sleep(8000); // para imprimir para 8 segundos
+        system("CLS"); //limpia la consola (borra todo) 
+    }
+
+};
+
+class ConsecuenciaPositiva : public Consecuencia {
+
+
+public:
+
+    ConsecuenciaPositiva() {
+        frases = {
+            "Mi circulacion se siente bien!",
+            "Oye, me siento bastante bien :D!",
+            "Badump... Badump... lo siento, lato mas fuerte cuando estoy feliz. >_<",
+            "> u <",
+            ":3",
+            "Me siento rojito :D",
+            "yeeeeyyyyy xD"
+        };
+    }
+
+    void mostrarFrase(const Emocion& emocion) override { //applica mostrar para consecuencias positivas 
+        Consecuencia::mostrarFrase(emocion);
+    }
+
+
+
+};
+
+class ConsecuenciaNegativa : public Consecuencia {
+
+
+
+public:
+
+    ConsecuenciaNegativa() {
+        frases = {
+            "Siento que mis arterias estan obstruidas...",
+            "Oye... no te importa como me siento?",
+            "Por que tienes que hacerme sentir asi...",
+            "Dx",
+            ");",
+            "ayudame... D:"
+        };
+    }
+
+    void mostrarFrase(const Emocion& emocion) override {
+        Consecuencia::mostrarFrase(emocion);
+    }
+
+};
 
 class Corazoncito {
 private:
@@ -63,7 +133,7 @@ public:
     string getColor() const { return color; }
     double getPeso() const { return peso; }
     double getAltura() const { return altura; }
-    Emocion getEmocion() const { return emocionDeCora; }
+    Emocion getEmocion() const { return EmocionDeCora; }
 
 
     void personalizarCora()
@@ -76,15 +146,15 @@ public:
 
     }
 
-    void evaluarEstado() const {
+    void evaluarEstado() const {  //funcion que llama los niveles de corazoncito
         int estres = EmocionDeCora.getNivelEstres();
         int animo = EmocionDeCora.getNivelAnimo();
 
-        if (estres < 6 || animo > 6) {
+        if (estres > 6 || animo < 6) { //dependiendo de su niveles, da una consecuencia negativa o positiva
             ConsecuenciaNegativa negativa;
             negativa.mostrarFrase(EmocionDeCora);
         }
-        else if (estres < 6 || animo > 6)  {
+        else {
             ConsecuenciaPositiva positiva;
             positiva.mostrarFrase(EmocionDeCora);
         }
@@ -92,6 +162,8 @@ public:
 
 
 };
+
+
 
 class Habito {
 
@@ -107,81 +179,17 @@ class Logro {
 };
 
 class Meta {
-    
-};
-
-class Consecuencia {
-protected:
-    vector<string> frases;
-
-public: 
-
-    Consecuencia() { } 
-
-    virtual ~Consecuencia() {}
-
-    virtual void mostrarFrase(const Emocion& emocion) {
-        int estres = emocion.getNivelEstres();
-        int animo = emocion.getNivelAnimo();
-
-            int indiceDeFrases = rand() % frases.size();
-            cout << frases[indiceDeFrases] << endl;
-    }
-    
-};
-
-class ConsecuenciaPositiva : public Consecuencia {
-   
-
-public:
-
-    ConsecuenciaPositiva() {
-        frases = {
-            "Mi circulacion se siente bien!",
-            "Oye, me siento bastante bien :D!",
-            "Badump... Badump... lo siento, lato mas fuerte cuando estoy feliz. >_<",
-            "> u <",
-            ":3",
-            "Me siento rojito :D"
-        };
-    }
-
-    void mostrarFrase(const Emocion& emocion) override {
-        Consecuencia::mostrarFrase(emocion);
-    }
-
-       
 
 };
 
-class ConsecuenciaNegativa : public Consecuencia {
 
-   
-
-public: 
-
-    ConsecuenciaNegativa() {
-        frases = {
-            "Siento que mis arterias estan obstruidas...",
-            "Oye... no te importa como me siento?",
-            "Por que tienes que hacerme sentir así...",
-            "Dx",
-            ");",
-            "ayudame... ):"
-        };
-    }
-
-    void mostrarFrase(const Emocion& emocion) override {
-        Consecuencia::mostrarFrase(emocion);
-    }
-
-};
 
 
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
+
 
